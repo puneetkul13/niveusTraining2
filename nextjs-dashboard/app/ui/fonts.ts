34037399b9b5824d939b5c7119f3478346fd269d
puneetkul13 <your_github_email>
2023-12-18 01:20:14 +0530

    changes

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google'
+export const inter = Inter({ subsets: ['latin'] });
+export const inter1 = Lusitana({subsets:['latin'], weight:"700"});
